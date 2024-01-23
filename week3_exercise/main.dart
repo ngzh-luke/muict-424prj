136be375c87a38abcd1713755fa9d51704301a3c
Luke <kittipich.a@gmail.com>
2024-01-23 12:15:17 +0700

    submit code

---
+++ week3_exercise/main.dart
@@ -0,0 +1,123 @@
+// lab 3
+// Kittipich Aiumbhornsin
+// 6488004
+
+import 'package:flutter/widgets.dart';
+
+main() => runApp(myApp());
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 60.0,
+          horizontal: 80.0,
+        ),
+        color: const Color(0xFFFFFFFF),
+        child: Content(),
+      ),
+    );
+  }
+}
+
+class Content extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text("Week 3 Exercise"),
+        Counter('6488004'),
+        Counter('Kittipich Aiumbhornsin'),
+        Counter("Travel Diary"),
+        Counter("6488089_6388092_6388154_6488168")
+      ],
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  final String _name;
+  Counter(this._name);
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10.0),
+      padding: EdgeInsets.all(4.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Color(0xFFFD6A02)),
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // [widget] is the property of the State class that stores
+          // the instance of the [StatefulWidget] ([Counter] in our case)
+          _CounterLabel(widget._name),
+          _CounterButton(
+            count,
+            onPressed: () {
+              setState(() {
+                ++count;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CounterLabel extends StatelessWidget {
+  static const textStyle = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 26.0,
+  );
+
+  final String _label;
+  _CounterLabel(this._label);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _label,
+      style: _CounterLabel.textStyle,
+    );
+  }
+}
+
+class _CounterButton extends StatelessWidget {
+  final count;
+  final onPressed;
+  _CounterButton(this.count, {@required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 6.0),
+        decoration: BoxDecoration(
+          color: Color(0xFFFD6A02),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        child: Center(
+          child: Text(
+            '$count',
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
