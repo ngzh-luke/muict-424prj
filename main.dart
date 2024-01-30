e50a63b573ac9b6b8f8e83895d08c10209ad4c00
Lr1zz <141892565+Lr1zz@users.noreply.github.com>
2024-01-30 12:14:43 +0700

    week 4 exercise

--- main.dart
+++ main.dart
@@ -1,120 +1,109 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-main() => runApp(App());
+void main() {
+  runApp(const MyApp());
+}
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: 60.0,
-          horizontal: 80.0,
-        ),
-        color: Color.fromARGB(255, 84, 248, 248),
-        child: Content(),
+    return MaterialApp(
+      title: 'Prodocuts Listing',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const MyHomePage(title: 'Prodocuts Listing'),
     );
   }
 }
 
-class Content extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Counter('6488168 Linfeng Zhang'),
-        Counter('Travel Diary'),
-        Counter('6488004'),
-        Counter('6488089'),
-        Counter('6388092'),
-        Counter('6388154'),
-      ],
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Products Listing"),
+          backgroundColor: Colors.green,
+        ),
+        body: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+          children: <Widget>[
+            ProductBox(
+                name: "iPhone",
+                description: "iPhone is the stylist phone ever",
+                price: 1000,
+                image: "iphone.jfif"),
+            ProductBox(
+                name: "Pixel",
+                description: "Pixel is the most featureful phone ever",
+                price: 800,
+                image: "pixel.jfif"),
+            ProductBox(
+                name: "Laptop",
+                description: "Laptop is most productive development tool",
+                price: 2000,
+                image: "laptop.jfif"),
+            ProductBox(
+                name: "Tablet",
+                description:
+                    "Tablet is the most useful device ever for meeting",
+                price: 1500,
+                image: "tablet.jfif"),
+            ProductBox(
+                name: "Pendrive",
+                description: "Pendrive is useful storage medium",
+                price: 100,
+                image: "pendrive.jfif"),
+            ProductBox(
+                name: "Floppy Drive",
+                description: "Floppy drive is useful rescue storage medium",
+                price: 20,
+                image: "floppy.png"),
+          ],
+        ));
   }
 }
 
-class Counter extends StatefulWidget {
-  final String _name;
-  Counter(this._name);
-
-  @override
-  State<Counter> createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int count = 0;
-
-  @override
+class ProductBox extends StatelessWidget {
+  ProductBox(
+      {super.key,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.image});
+  final String name;
+  final String description;
+  final int price;
+  final String image;
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 10.0),
-      padding: EdgeInsets.all(4.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Color.fromARGB(255, 212, 91, 152)),
-        borderRadius: BorderRadius.circular(4.0),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // [widget] is the property of the State class that stores
-          // the instance of the [StatefulWidget] ([Counter] in our case)
-          _CounterLabel(widget._name),
-          _CounterButton(
-            count,
-            onPressed: () {
-              setState(() {
-                ++count;
-              });
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _CounterLabel extends StatelessWidget {
-  static const textStyle = TextStyle(
-    color: Color(0xFF000000),
-    fontSize: 26.0,
-  );
-
-  final String _label;
-  _CounterLabel(this._label);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      _label,
-      style: _CounterLabel.textStyle,
-    );
-  }
-}
-
-class _CounterButton extends StatelessWidget {
-  final count;
-  final onPressed;
-  _CounterButton(this.count, {@required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 6.0),
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 207, 73, 185),
-          borderRadius: BorderRadius.circular(4.0),
-        ),
-        child: Center(
-          child: Text(
-            '$count',
-            style: TextStyle(fontSize: 20.0),
-          ),
-        ),
-      ),
-    );
+        padding: EdgeInsets.all(2),
+        height: 150,
+        child: Card(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+              Image.asset("assets/" + image),
+              Expanded(
+                  child: Container(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(this.name,
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(this.description),
+                          Text("Price: " + this.price.toString()),
+                        ],
+                      )))
+            ])));
   }
 }
